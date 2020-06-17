36f1a312b35c9bef233a5481c8a0d3c956a19988
Max Starkman <max.starkman@gmail.com>
2020-06-17 17:29:53 -0400

    finished studio
    Signed-off-by: Max Starkman <max.starkman@gmail.com>

--- src/org/launchcode/java/studios/areaofcircle/Area.java
+++ src/org/launchcode/java/studios/areaofcircle/Area.java
@@ -5,8 +5,25 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("What is the radius?: ");
-        Double radius = input.nextDouble();
+        Boolean invalidInput;
+        Double radius = null;
+
+        do {
+            System.out.println("What is the radius?: ");
+            try {
+                radius = input.nextDouble();
+            } catch (Exception e) {
+                System.out.println("Enter a valid radius!");
+            }
+
+            if (radius < 0) {
+                System.out.println("Enter a positive value radius!");
+                invalidInput = true;
+            } else {
+                invalidInput = false;
+            }
+        } while (invalidInput);
+
         System.out.println("Area is " + Circle.getArea(radius));
     }
 }
